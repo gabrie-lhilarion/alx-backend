ed92938c3964b54238377060c5d77440afc88911
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-07-31 04:59:30 +0100

    Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+A basic Flask application with a single route and an index.html template.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """
+    Renders the index.html template which displays a welcome message.
+
+    Returns:
+        str: The rendered HTML content of the index.html template.
+    """
+    return render_template('index.html')
